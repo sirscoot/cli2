842c2ae23dfa431f97e7d0538d0ae4452dccb7e3
sirscoot <dmskeletor@gmail.com>
2020-01-08 15:15:54 -0800

    updated

--- cli.py
+++ cli.py
@@ -8,32 +8,46 @@ c = conn.cursor()
 
 
 #filters all transactions based on customer id
-def get_transaction_by_customer_id(args):
-    c.execute(r"SELECT * FROM transactions WHERE customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)", [args])
-    return c.fetchmany(5)
+def get_transaction_by_customer_id(id):
+    c.execute(r"SELECT * FROM transactions WHERE customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)", [id])
+    output = c.fetchmany(15)
     conn.close()
+    return output
+    
 
 #shows all transactions filtered by customer name
-def get_transactions_by_name(args):
-    c.execute(r"SELECT * FROM transactions WHERE customer_name=?", [args])
-    return c.fetchmany(5)
+def get_transactions_by_name(name):
+    c.execute(r"SELECT * FROM transactions WHERE customer_name=?", [name])
+    output = c.fetchmany(15)
     conn.close()
+    return output
 
 #shows all transactions filted by date
-def get_transaction_by_date(args):
-    c.execute(r"SELECT * from transactions WHERE order_date=?", [args])
-    return c.fetchmany(5)
+def get_transaction_by_date(date):
+    c.execute(r"SELECT * from transactions WHERE order_date=?", [date])
+    output = c.fetchmany(15)
     conn.close()
+    return output
+
+def write_to_csv(args):
+    c.execute(r"SELECT * from transactions")
+    output = c.fetchmany(15)
+    conn.close()
+    return output
 
 def main():
     #create the parser argument
     parser = argparse.ArgumentParser(description="Transaction manager")
+    group = parser.add_mutually_exclusive_group()
 
-    parser.add_argument("-n", "--name", type=str, help="Filters transactions based on date")
 
-    parser.add_argument("-i", "--id", type=int, help="Filters transactions based customer id")
+    group.add_argument("-n", "--name", type=str, help="Filters transactions based on date")
+
+    group.add_argument("-i", "--id", type=int, help="Filters transactions based customer id")
 
     parser.add_argument("-d", "--date", type=str, help="Filters transactions based on order date")
+
+    parser.add_argument("-w", "--write", action="store_true", help="Write data to csv file")
 
     args = parser.parse_args()
 
@@ -41,10 +55,14 @@ def main():
 
     if args.name:
         print(get_transactions_by_name(args.name))
-    elif args.id:
+    if args.id:
         print(get_transaction_by_customer_id(args.id))
-    elif args.date:
+    if args.date:
         print(get_transaction_by_date(args.date))
+    if args.write:
+        f = open("transactions.csv", "a")
+        f.write(str(write_to_csv(args)))
+        print("Data saved")
 
 
 
