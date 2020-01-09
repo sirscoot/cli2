fcce596afa6200d8e65fa52d08991da9dd19feb7
sirscoot <dmskeletor@gmail.com>
2020-01-09 03:57:04 -0800

    update

--- cli.py
+++ cli.py
@@ -10,29 +10,26 @@ import csv
 #filters all transactions based on customer id
 def get_transaction_by_customer_id(c, id):
     c.execute(r"SELECT * FROM transactions WHERE customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)", [id])
-    write_to_csv(c)
+    c.fetchall()
 
 #shows all transactions filtered by customer name
 def get_transactions_by_name(c, name):
     c.execute(r"SELECT * FROM transactions WHERE customer_name=?", [name])
-    write_to_csv(c)
-
+    c.fetchall()
 
 #shows all transactions filted by date
 def get_transaction_by_date(c, date):
     c.execute(r"SELECT * from transactions WHERE order_date=?", [date])
-    write_to_csv(c)
-
-def write_to_csv(c):
-    with open('results.csv', 'w', newline='') as csvfile:
-        csvWriter = csv.writer(csvfile)
-        csvWriter.writerow(['Transactions:'])
-        csvWriter.writerows(c.fetchmany(15))
-
+    c.fetchall()
       
 def main():
     conn = db.connect('transactions-2010.db')
     c = conn.cursor()
+
+    def write_to_csv(c):
+        with open('results.csv', 'w', newline='') as csvfile:
+            csvWriter = csv.writer(csvfile)
+            csvWriter.writerow(['Transactions:'])
 
     #create the parser argument
     parser = argparse.ArgumentParser(description="Transaction manager")
@@ -51,13 +48,13 @@ def main():
 
 
     if args.name:
-        print(get_transactions_by_name(c, args.name))
+        return get_transactions_by_name(c, args.name)
         print("Data written to file")
     if args.id:
-        print(get_transaction_by_customer_id(c, args.id))
+        return get_transaction_by_customer_id(c, args.id)
         print("Data written to file")
     if args.date:
-        print(get_transaction_by_date(c, args.date))
+        return get_transaction_by_date(c, args.date)
         print("Data written to file")
 
     conn.close()
