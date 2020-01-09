8936eb20910aabb92a4d2dadac4acc64036f15d0
sirscoot <dmskeletor@gmail.com>
2020-01-09 03:32:25 -0800

    updated

--- cli.py
+++ cli.py
@@ -11,29 +11,26 @@ c = conn.cursor()
 #filters all transactions based on customer id
 def get_transaction_by_customer_id(id):
     c.execute(r"SELECT * FROM transactions WHERE customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)", [id])
-    with open('transactions_id.csv', 'w', newline='') as csvfile:
-        csvWriter = csv.writer(csvfile)
-        csvWriter.writerow(['Transactions by id:'])
-        csvWriter.writerows(c.fetchmany(15))
+    write_to_csv()
 
 #shows all transactions filtered by customer name
 def get_transactions_by_name(name):
     c.execute(r"SELECT * FROM transactions WHERE customer_name=?", [name])
-    with open('transactions_name.csv', 'w', newline='') as csvfile:
-        csvWriter = csv.writer(csvfile)
-        csvWriter.writerow(['Transactions by name:'])
-        csvWriter.writerows(c.fetchmany(15))
+    write_to_csv()
 
 
 #shows all transactions filted by date
 def get_transaction_by_date(date):
     c.execute(r"SELECT * from transactions WHERE order_date=?", [date])
-    with open('transactions_date.csv', 'w', newline='') as csvfile:
+    write_to_csv()
+
+def write_to_csv():
+    with open('results.csv', 'w', newline='') as csvfile:
         csvWriter = csv.writer(csvfile)
-        csvWriter.writerow(['Transactions by date:'])
+        csvWriter.writerow(['Transactions:'])
         csvWriter.writerows(c.fetchmany(15))
 
-        
+      
 def main():
     #create the parser argument
     parser = argparse.ArgumentParser(description="Transaction manager")
