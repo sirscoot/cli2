483015146a1b68b7c611bd60c8215a5eab24bb4a
sirscoot <dmskeletor@gmail.com>
2020-02-24 07:05:57 -0800

    updated

--- cli.py
+++ cli.py
@@ -3,42 +3,31 @@ import datetime
 import sqlite3 as db
 import csv
 
+def query_data(c, date_range=None, customer_name=None, customer_id=None, customer_date=None):
 
+    query_strings = list()
+    query_args = list()
 
-def query_data(c, dateRange=None, customer_name=None, customer_id=None, customer_date=None):
+    if customer_name is not None:
+        query_strings.append('customer_name=?')
+        query_args.append(customer_name)
 
-    # stuck on customer id
     if customer_id is not None:
-        query_where = 'order_date=DATE(?)'
-        query_args = [customer_date]
-        query_where = f'{query_where} AND customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)'
+        query_strings.append('customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)')
         query_args.append(customer_id)
 
     if customer_date is not None:
-        query_where = 'order_date=DATE(?)'
-        query_args = [customer_date]
-        query_where = f'{query_where} AND customer_name=?'
-        query_args.append(customer_name)
+        query_strings.append('order_date=?')
+        query_args.append(customer_date)
 
-    if customer_name and dateRange is not None:
-        split_date = dateRange.split(",")
-        query_where = 'order_date BETWEEN DATE(?) AND DATE(?)'
-        query_args = [split_date[0], split_date[1]]
-        query_where = f'{query_where} AND customer_name=?'
-        query_args.append(customer_name)
+    if date_range is not None:
+        split_date = date_range.split(",")
+        query_strings.append('order_date BETWEEN DATE(?) AND DATE(?)')
+        query_args.extend([split_date[0], split_date[1]])
 
-    if customer_id and dateRange is not None:
-        split_date = dateRange.split(",")
-        query_where = 'order_date BETWEEN DATE(?) AND DATE(?)'
-        query_args = [split_date[0], split_date[1]]
-        query_where = f'{query_where} AND customer_name=(SELECT customer_name FROM customers WHERE customer_id=?)'
-        query_args.append(customer_id)
-
-
-    query_string = f"SELECT * FROM transactions WHERE {query_where}"
+    query_string = f"SELECT * FROM transactions WHERE {' AND '.join(query_strings)}"
     c.execute(query_string, query_args)
     return c.fetchall()
-
 
 # writes data to csv file
 def write_to_csv(rows, output):
@@ -54,16 +43,14 @@ def write_to_csv(rows, output):
             row_list[2] = whole_price
             csvWriter.writerow(row_list)
 
-
 if __name__ == "__main__":
     # create the parser argument
     parser = argparse.ArgumentParser(description="Transaction manager")
     group = parser.add_mutually_exclusive_group()
     req_group = parser.add_mutually_exclusive_group()
 
-
     # parser argument
-    group.add_argument("-n", "--name", type=str, help="Filters transactions based on date")
+    group.add_argument("-n", "--name", type=str, help="Filters transactions based on customer name")
     group.add_argument("-i", "--id", type=int, help="Filters transactions based customer id")
 
     req_group.add_argument("-d", "--date", type=str, help="Filters transactions based on order date")
@@ -72,28 +59,12 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", type=str, default='results.csv', help="Changes the default file name to desired file name")
     parser.add_argument("-f", "--filename", type=str, help="Opens selected database file", required=True)
 
-
     args = parser.parse_args()
 
     conn = db.connect(args.filename)
     c = conn.cursor()
 
-    output_data = None
-
-    if args.id and args.date:
-        output_data = query_data(c, customer_id=args.id, customer_date=args.date)
-
-
-    if args.name and args.date:
-        output_data = query_data(c, customer_name=args.name, customer_date=args.date)
-
-
-    if args.daterange and args.name:
-        output_data = query_data(c, args.daterange, customer_name=args.name)
-    if args.daterange and args.id:
-        output_data = query_data(c, args.daterange, customer_id=args.id)
-    if args.daterange and not args.id and not args.name:
-        output_data = query_data(c, args.daterange)
+    output_data = query_data(c, customer_id=args.id, customer_name=args.name, customer_date=args.date, date_range=args.daterange)
 
     write_to_csv(output_data, args.output)
     conn.close()
